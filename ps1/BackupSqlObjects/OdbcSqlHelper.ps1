10056c5542177aea2537573afc716c156504ee29
Erik Plachta <ErikPlachta@users.noreply.github.com>
2025-04-16 17:54:28 -0400

    fix

--- ps1/BackupSqlObjects/OdbcSqlHelper.ps1
+++ ps1/BackupSqlObjects/OdbcSqlHelper.ps1
@@ -1,25 +1,4 @@
 function Get-OdbcConnection {
-    <#
-    .SYNOPSIS
-        Creates and returns an ODBC connection to Azure SQL using SSO (Active Directory Integrated Authentication).
-
-    .DESCRIPTION
-        Uses the ODBC Driver 17 or 18 for SQL Server to connect to an Azure SQL Database using
-        Active Directory Integrated authentication (SSO on Azure AD-joined machines).
-
-    .PARAMETER ServerName
-        The fully qualified Azure SQL Server name (e.g., your-server.database.windows.net)
-
-    .PARAMETER DatabaseName
-        The name of the database to connect to.
-
-    .OUTPUTS
-        System.Data.Odbc.OdbcConnection
-
-    .EXAMPLE
-        $conn = Get-OdbcConnection -ServerName "your-server.database.windows.net" -DatabaseName "your-db"
-    #>
-
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
@@ -37,19 +16,20 @@ Server=tcp:$ServerName;
 Database=$DatabaseName;
 Authentication=ActiveDirectoryIntegrated;
 Encrypt=yes;
-TrustServerCertificate=no;
+TrustServerCertificate=yes;
 "@
 
     try {
-        $conn = New-Object System.Data.Odbc.OdbcConnection
-        $conn.ConnectionString = $connectionString
+        $conn = New-Object System.Data.Odbc.OdbcConnection $connectionString
         $conn.Open()
+        Write-Host "Connection successful to $ServerName\$DatabaseName" -ForegroundColor Green
         return $conn
     } catch {
-        Write-Error "ODBC connection failed: $_"
+        Write-Error "ODBC connection failed: $($_.Exception.Message)"
         return $null
     }
 }
+
 
 function Invoke-OdbcQuery {
     <#
